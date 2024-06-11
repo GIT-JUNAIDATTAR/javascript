e43c4de1b69a304273cd920d921177eef4a219e3
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-11 21:12:30 +0530

    chapter 2 practice set question 1 solved

--- v-7_conditionExpressionChapter 2-ps.js
+++ v-7_conditionExpressionChapter 2-ps.js
@@ -1,1 +1,9 @@
-//
+//use the logical operator to find whether the age of person lies bitweenn 10 and 20.
+
+let age1 = prompt (" enter your age please--->>>");
+if(age1 > 10 && age1 < 20) {
+    console.log("your age is bitween 10 to 20")
+}
+else{
+    console.log("you age is not bitween 10 to 20")
+}
