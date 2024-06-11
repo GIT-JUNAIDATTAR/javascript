e496bd6da6a910071e7261b526e82d365130cd96
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-11 21:23:44 +0530

    chapter 2 practice set  question 2 solved

--- v-7_conditionExpressionChapter 2-ps.js
+++ v-7_conditionExpressionChapter 2-ps.js
@@ -1,9 +1,25 @@
-//use the logical operator to find whether the age of person lies bitweenn 10 and 20.
+// 1.use the logical operator to find whether the age of person lies bitweenn 10 and 20.
 
-let age1 = prompt (" enter your age please--->>>");
+let age1 = 15;//prompt (" enter your age please--->>>");
 if(age1 > 10 && age1 < 20) {
     console.log("your age is bitween 10 to 20")
 }
 else{
     console.log("you age is not bitween 10 to 20")
+}
+
+//2.demonstrate the use of the switch case statement in javascript 
+
+let age2 = 15;//prompt (" enter your age please--->>>");
+
+switch(age2){
+    case 11 :console.log("your age is 11"); break;
+    case 12 :console.log("your age is 12"); break;
+    case 13 :console.log("your age is 13"); break;
+    case 14 :console.log("your age is 14"); break;
+    case 15 :console.log("your age is 15"); break;
+    case 16 :console.log("your age is 16"); break;
+    case 17 :console.log("your age is 17"); break;
+    case 18 :console.log("your age is 18"); break;
+    case 19 :console.log("your age is 19"); break;
 }
