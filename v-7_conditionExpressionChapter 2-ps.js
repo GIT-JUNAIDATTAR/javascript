8ae36a476917b703a62999a8fc3580a116cacaab
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-13 12:36:46 +0530

    chapter 2 example 3 is solved in this commit

--- v-7_conditionExpressionChapter 2-ps.js
+++ v-7_conditionExpressionChapter 2-ps.js
@@ -1,4 +1,5 @@
 // 1.use the logical operator to find whether the age of person lies bitweenn 10 and 20.
+//this is example of or "&&" logical operator,in this "&&" chek all statement is true or not?,if true it exicuted,if one of these is false it does not executed
 
 let age1 = 15;//prompt (" enter your age please--->>>");
 if(age1 > 10 && age1 < 20) {
@@ -8,7 +9,8 @@ else{
     console.log("you age is not bitween 10 to 20")
 }
 
-//2.demonstrate the use of the switch case statement in javascript 
+//2.demonstrate the use of the switch case statement in javascript
+//use of switch case
 
 let age2 = 15;//prompt (" enter your age please--->>>");
 
@@ -22,4 +24,16 @@ switch(age2){
     case 17 :console.log("your age is 17"); break;
     case 18 :console.log("your age is 18"); break;
     case 19 :console.log("your age is 19"); break;
+}
+
+//3. write a javascript program to find whether a number divisible by 2 or 3 
+  
+// and "||" logical operator is use in this example ,if one statement is correct it is exicuted
+let num =5;
+
+if(num % 2==0 || num % 3==0){
+    console.log("this number is divisible by 2 and 3");
+}
+else{
+    console.log("this number is not divisible by 2 and 3");
 }
