1e0ca52968d57f4066d70c50b2f3ca641cc7c5df
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-13 13:09:20 +0530

    adding prompt for user input in all examples, chapter 2 is over next chapter will be continue

--- v-7_conditionExpressionChapter 2-ps.js
+++ v-7_conditionExpressionChapter 2-ps.js
@@ -1,7 +1,7 @@
 // 1.use the logical operator to find whether the age of person lies bitweenn 10 and 20.
 //this is example of or "&&" logical operator,in this "&&" chek all statement is true or not?,if true it exicuted,if one of these is false it does not executed
 
-let age1 = 15;//prompt (" enter your age please--->>>");
+let age1 = prompt (" enter your age please--->>>");
 if(age1 > 10 && age1 < 20) {
     console.log("your age is bitween 10 to 20")
 }
@@ -12,7 +12,7 @@ else{
 //2.demonstrate the use of the switch case statement in javascript
 //use of switch case
 
-let age2 = 15;//prompt (" enter your age please--->>>");
+let age2 = prompt (" enter your age please--->>>");
 
 switch(age2){
     case 11 :console.log("your age is 11"); break;
@@ -29,7 +29,8 @@ switch(age2){
 //3. write a javascript program to find whether a number divisible by 2 or 3 
   
 // and "||" logical operator is use in this example ,if one statement is correct it is exicuted
-let num =5;
+
+let num = prompt (" enter your number please--->>>");
 
 if(num % 2==0 || num % 3==0){
     console.log("this number is divisible by 2 and 3");
@@ -41,7 +42,7 @@ else{
 //4.write a javaascript program to find ither a number divisible by 2 or 3.
 // in this example we have to use "||" logical operator , if one of statement is true the program will be executed.
 
-let num2 = 2;
+let num2 = prompt (" enter your number please--->>>");
 
 if (num2 % 2==0 || num2 % 3==0){
     console.log("this number is divisible by either 2 or 3");
@@ -54,6 +55,6 @@ else{
 //5.print "you can drive " or " you cannot drive " based on age being greater than 18 using ternary operator
 //in this we are using ternary operator 
 
-let age3 = 17;
+let age3 = prompt (" enter your age please--->>>");
 
 console.log( age3 <= 18 ? "you cannot drive" : "you can drive");
