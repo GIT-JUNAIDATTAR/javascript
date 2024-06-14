be2f02ae1161c286c208de8d5bf1dc38de31a264
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-14 17:01:13 +0530

    examples of for loop

--- v-9_chapter-3-ForLoop.js
+++ v-9_chapter-3-ForLoop.js
@@ -3,3 +3,15 @@
 for( let i = 0 ; i<=5 ; i++){
     console.log(i);
 }
+
+// writing the program to add n natural number
+
+let sum = 0;
+let num = 4;
+for(let i = 0;i < num; i++){
+    sum = sum + (i+1);
+    
+}    
+
+console.log("the sum of "+ num + " is "+ sum);
+
