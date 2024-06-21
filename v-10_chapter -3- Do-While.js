1f3170592fbc4a1ed0cb00645ed0f3358a601554
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-21 17:28:51 +0530

    example of do while loop

--- v-10_chapter -3- Do-While.js
+++ v-10_chapter -3- Do-While.js
@@ -1,4 +1,5 @@
-
+//example of while loop 
+/*
 let n = prompt("enter your numner");
 n = Number.parseInt(n);
 let i = 1;
@@ -6,3 +7,17 @@ while(i<=n ){//in while loop if statement iss true it enter innto the while loop
     console.log(i);
     i++
 }
+    */
+
+// example of do - while loop
+
+
+//let n = prompt("enter your numner");
+//n = Number.parseInt(n);
+let n2 = 5
+let i = 1;
+do {
+console.log(i);
+i++
+}
+while(i<=n2)
