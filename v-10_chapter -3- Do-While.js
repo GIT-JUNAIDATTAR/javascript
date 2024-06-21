e5b8511e189696680ef1afa5d24d589637282bfc
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-21 17:16:28 +0530

    removing the comment from user input , i test the code by direct declaring the number because prompt not working on vs code ide

--- v-10_chapter -3- Do-While.js
+++ v-10_chapter -3- Do-While.js
@@ -1,6 +1,6 @@
-//let n = prompt("enter your numner");
-//n = Number.parseInt(n);
-let n = 5;
+
+let n = prompt("enter your numner");
+n = Number.parseInt(n);
 let i = 1;
 while(i<=n ){
     console.log(i);
