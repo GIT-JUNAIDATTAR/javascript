e444061b8375a7c7be59e571511f94cca82ea704
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-21 17:18:54 +0530

    comment realated to while loop

--- v-10_chapter -3- Do-While.js
+++ v-10_chapter -3- Do-While.js
@@ -2,7 +2,7 @@
 let n = prompt("enter your numner");
 n = Number.parseInt(n);
 let i = 1;
-while(i<=n ){
+while(i<=n ){//in while loop if statement iss true it enter innto the while loop
     console.log(i);
     i++
 }
