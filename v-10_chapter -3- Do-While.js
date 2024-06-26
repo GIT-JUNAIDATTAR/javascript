8721a6c434a586a1bac71da51eacbb148f701ac7
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-06-26 15:28:53 +0530

    removing all comments

--- v-10_chapter -3- Do-While.js
+++ v-10_chapter -3- Do-While.js
@@ -1,5 +1,5 @@
 //example of while loop 
-/*
+
 let n = prompt("enter your numner");
 n = Number.parseInt(n);
 let i = 1;
@@ -7,7 +7,7 @@ while(i<=n ){//in while loop if statement iss true it enter innto the while loop
     console.log(i);
     i++
 }
-    */
+    
 
 // example of do - while loop
 
@@ -15,9 +15,9 @@ while(i<=n ){//in while loop if statement iss true it enter innto the while loop
 //let n = prompt("enter your numner");
 //n = Number.parseInt(n);
 let n2 = 5
-let i = 1;
+let i2 = 1;
 do {
-console.log(i);
-i++
+console.log(i2);
+i2++
 }
-while(i<=n2)
+while(i2<=n2)
