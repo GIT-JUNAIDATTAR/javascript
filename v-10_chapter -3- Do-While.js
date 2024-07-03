be6757fae19f44baba40f492c70ebf10667ef16d
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-03 15:26:56 +0530

    uncoment the user input

--- v-10_chapter -3- Do-While.js
+++ v-10_chapter -3- Do-While.js
@@ -12,9 +12,9 @@ while(i<=n ){//in while loop if statement iss true it enter innto the while loop
 // example of do - while loop
 
 
-//let n = prompt("enter your numner");
-//n = Number.parseInt(n);
-let n2 = 5
+let n2 = prompt("enter your numner");
+n2 = Number.parseInt(n2);
+
 let i2 = 1;
 do {
 console.log(i2);
