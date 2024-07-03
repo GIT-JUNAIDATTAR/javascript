c25f4fd2de8584a947f6a402628e36ee372bd066
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-03 17:30:10 +0530

    correcting the incorrect anser and cheking once again

--- v-9_chapter-3-ForLoop-ps.js
+++ v-9_chapter-3-ForLoop-ps.js
@@ -1,38 +1,44 @@
 //1. write a program  for print mark of student in object 
 
-let obj ={
+let marks = {
     harry : 98,
-    rohan : 70,
-    akash : 7
+    mike : 70,
+    shon : 30,
+    tony : 40,
+    gracy : 50
+}
+//if we use for loop for printing the marks, we have to write this type of big syntax
+for(let i = 0;i < Object.keys(marks).length;i++ ){
+    console.log("the marks of " + Object.keys(marks)[i] + " are -->> " + marks[Object.keys(marks)[i]]);
 }
 
-//2. use for-in loop to print the mark of student in question number one
 
-for(let mark in obj){
-    console.log(obj[mark]);
-}
-
-//3. write a program to print "try again " until user not enter correct number
-
-let num = 11;
+// //2. use for-in loop to print the mark of student in question number one
+// //if we use for-in loop in the code is very simple and short 
+// for(let i in marks){
+//     console.log(" THE MARKS OF " + i + " ARE --->>>"+ marks[i]);
+// }
 
 
-    if(num>0 && num<=10){
-        console.log("corect number");
-    }
+// //3. write a program to print "try again " until user not enter correct number
 
-    else{
-        console.log("try again");
-    }
+// let num = prompt("enter your number");
+// let correctnum = 12;
+
+// while( num != correctnum ){
+//     console.log("not correct number , try again")
+//     num = prompt("enter your number");
+//     num = parseInt(num);
+// }
+// console.log("congrats this is correct number");
+
 
  //4. write a function to find the mean of 5 numbers
 
- let num1 =1 , num2 = 2, num3 = 3, num4 = 4, num5 = 5;
-
- let totaln = num1 + num2 + num3 + num4+ num5;
-
- let mean = totaln / 5;
-
-  console.log("Mean of given  five numbers is --->>> " + mean);
+//  const findmean = (a,b,c,d,e ) => {
+//     return  (a + b + c + d )/4;
+//  }
+ 
+//   console.log( findmean(4,5,6,7));
  
  
