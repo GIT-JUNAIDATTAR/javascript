a31b89a7feaebcb1a1b3bae3a4ffb170972a4129
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-04 20:29:45 +0530

    if we have to calculate the avrage of numbers without using function

---
+++ v-11_chapter-3-function.js
@@ -0,0 +1,9 @@
+// if we have to calculate the avrage of numbers without using function
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("Average of a and b is -->>" +(a + b )/ 2)
+console.log("Average of c and b is -->>" +(c + b )/ 2)
+console.log("Average of a and c is -->>" +(a + c )/ 2)
