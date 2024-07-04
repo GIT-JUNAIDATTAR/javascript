df0dbf70be2df1cab1d9899b376792eaa9c8758e
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-04 20:51:38 +0530

    i write basic program by using funtion and  i write the use of function

--- v-11_chapter-3-function.js
+++ v-11_chapter-3-function.js
@@ -7,3 +7,19 @@ let c = 3;
 console.log("Average of a and b is -->>" +(a + b )/ 2)
 console.log("Average of c and b is -->>" +(c + b )/ 2)
 console.log("Average of a and c is -->>" +(a + c )/ 2)
+
+//In downside code we will use the function to calculate the avarage of numbers
+
+function avg  (x , y) {
+    return (x + y)/2; // when we change anything in funnction , it can be reflect  wherever we use this function
+}
+
+let d = 4;
+let e = 5;
+let f = 6;
+
+console.log("average of d and e is --->>>"+ avg(d,e));
+console.log("average of  f and e is --->>>"+ avg(f,e));
+console.log("average of d and f --->>>"+ avg(d,f));
+
+// by decalaring function we use the logic of code one time and and we can use it multiple time by calling those function, and it saves our time
