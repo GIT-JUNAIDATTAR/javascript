ba9d30dbe6c2bbf1e020e9f27f6dbd8c5281c9a9
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-06 10:37:13 +0530

    for understand the Template Literal

--- v-13_IntroductionToString.js
+++ v-13_IntroductionToString.js
@@ -8,7 +8,22 @@ let surname = 'attar'; // we can also write sting in this way
 // if we want to convert any number ex-"7" into string we can change the type of value in string by using - toString();
 
 let a = 7;
-a.toString();
-console.log(typeof(a)); //typeof is to identify data type
+let b = a.toString();
+console.log(typeof(b)); //typeof is to identify data type
                         // output --->>> string
 //by this way we can change the number value into the string value
+
+//Template Literals
+
+//You can also use backticks ` (it is the button under your escape key) to create a string, called a template literal. This is useful when you need to embed variables within a string. Without template literals, you would need to concatenate strings and variables using the + operator. Here's an example:
+
+// when we dont use template literals
+
+let n = "mike";
+let f = "mango";
+
+console.log("hello my name is " + n + " and my favorite fruit is " + f);
+
+// when we use template literal
+
+console.log(`HELLO MY NAME IS ${n} AND MY FAVORITE FRUIT IS ${f}`);
