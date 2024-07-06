046c9120a98cd1eccb3c035372228f608b6732c6
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-06 10:13:03 +0530

    example is for , when we want to convert the number into the string

--- v-13_IntroductionToString.js
+++ v-13_IntroductionToString.js
@@ -5,3 +5,10 @@ let name = "junaid"; //we write string in this way
 let surname = 'attar'; // we can also write sting in this way
 //let wrongstring = " wrong way '; // this is incorrect way to write the string
 
+// if we want to convert any number ex-"7" into string we can change the type of value in string by using - toString();
+
+let a = 7;
+a.toString();
+console.log(typeof(a)); //typeof is to identify data type
+                        // output --->>> string
+//by this way we can change the number value into the string value
