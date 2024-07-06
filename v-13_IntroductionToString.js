72baeb45b0663fe672c2c47524a2e4d0665ab030
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-06 09:24:30 +0530

    basic example of declaring string inwrite way

---
+++ v-13_IntroductionToString.js
@@ -0,0 +1,7 @@
+
+//A string is a collection of characters enclosed in quotes. In JavaScript, you can use either single quotes or double quotes to create a string.
+
+let name = "junaid"; //we write string in this way
+let surname = 'attar'; // we can also write sting in this way
+//let wrongstring = " wrong way '; // this is incorrect way to write the string
+
