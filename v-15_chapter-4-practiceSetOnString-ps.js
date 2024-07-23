b053762680cffb60692d8be20226387e7c354f46
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-23 23:03:46 +0530

    i  am adding more examples of starts with function examples for better practice, tommrow i will practice the remaining questions

--- v-15_chapter-4-practiceSetOnString-ps.js
+++ v-15_chapter-4-practiceSetOnString-ps.js
@@ -7,6 +7,18 @@ console.log("har\"".length); //ans is -->>4
 
 const a ="cat want to drink milk";
 
-const b ="water";
+const b ="milk";
 
 console.log(`${b} ${a.includes(b) ? "is in" : "is not in"}  the sentence`);
+
+console.log(`${a.startsWith("c")}`); // its output is - true a is start with "c".
+
+console.log(`${a.startsWith("C")}`); // in this it return - false because this function is case sensitive
+
+console.log(`${a.endsWith("milk")}`);//its output is - true because its ends with "milk".
+
+console.log(`${a.startsWith("m" , 18)}`) // output is -true ( because m is on index [18] in a)
+
+console.log(`${a.startsWith("cat" , 1)}`) // output is -false (because its start cheking from index [1] c is in index[0]) 
+
+console.log(`${a.startsWith("at" , 1)}`) // output is -true (because its start cheking from index [1]) 
