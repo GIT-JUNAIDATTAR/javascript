67332bc7e4a28ac1eeebac6ac2e6133e4993a98c
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-23 22:10:20 +0530

    second question of practice set chapter 4

--- v-15_chapter-4-practiceSetOnString-ps.js
+++ v-15_chapter-4-practiceSetOnString-ps.js
@@ -2,3 +2,11 @@
 
 console.log("har\"".length); //ans is -->>4
 
+
+// 2.explore the include ,startwith, end with functions of string
+
+const a ="cat want to drink milk";
+
+const b ="water";
+
+console.log(`${b} ${a.includes(b) ? "is in" : "is not in"}  the sentence`);
