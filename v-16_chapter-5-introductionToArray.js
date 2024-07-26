be21c196ffc7d5b4fa25468dce66acd214ff8b37
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-26 18:28:22 +0530

    we can also channge the value of array

--- v-16_chapter-5-introductionToArray.js
+++ v-16_chapter-5-introductionToArray.js
@@ -16,4 +16,7 @@ console.log(name.length); // length always counnt eelements , starts from 1
 name[4]= "anas";
 console.log(name[4]); // we can add more values after
 
+name[0]= "junaid"; // we can also change the value of array 
+console.log(name[0]);
+
 let a =["name",8,2.2,"a"] // diffrent type of values we can insert in array
