4a69dbb9b883b8f5157595e80c343a13cb49b1dd
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-26 18:04:52 +0530

    printing length of arrays

--- v-16_chapter-5-introductionToArray.js
+++ v-16_chapter-5-introductionToArray.js
@@ -3,7 +3,10 @@
 
 let name = [ "mike " , "michel" , "harry" , "carry"];
 console.log(name);
+
 console.log(name[0]);
 console.log(name[1]);
 console.log(name[2]);
 console.log(name[3]);
+
+console.log(name.length); // length always counnt eelements , starts from 1
