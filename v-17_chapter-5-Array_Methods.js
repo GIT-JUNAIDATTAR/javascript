2f12dfa5c24f611ccc893359b5676f843a0ec292
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-07-30 19:04:59 +0530

    giving proper title to the methods example

--- v-17_chapter-5-Array_Methods.js
+++ v-17_chapter-5-Array_Methods.js
@@ -1,8 +1,12 @@
 
+// to.String() method of array
+
 let a = [1,2,3,4,5];
 let b = a.toString(); // By using toString() method of array, we can converts any datatype into String
 console.log(b);
 console.log(typeof b);
+
+// join() method of array
 
  let c = a.join("->"); // by using join() method of array,we can join all the array element using a seprator
  console.log(c);
