018d480d973e74a44d066b7bd4d53a9742f12a06
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-01 14:45:26 +0530

    delete operator of array

---
+++ v-18_chapter-5-more_Array_Methods.js
@@ -0,0 +1,8 @@
+
+// delete operator of array
+
+let num =[1,2,3,4,5,6,7,8,9,10];
+console.log(num.length);
+delete num[0];            // by using the delete operator of array we can delete the element of array
+console.log(num);
+console.log(num.length);   // but the length of array is not deleted //-imp
