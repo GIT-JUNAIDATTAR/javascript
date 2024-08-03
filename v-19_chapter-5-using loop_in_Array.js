559d8357c8582caadf885ae182220f1a1de4bc06
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-03 15:11:08 +0530

    classical for loop

--- v-19_chapter-5-using loop_in_Array.js
+++ v-19_chapter-5-using loop_in_Array.js
@@ -2,3 +2,12 @@
 //Looping through array - 
 //                        arrays can be looped through using the classical javascript for loop and 
 //                        through other method discuss below 
+
+
+//Classical For Loop -
+
+let num = [1,2,3,4,5];
+for(i=0;i<num.length;i++){
+    console.log(num [i]); //output : 1,2,3,4,5
+}
+
