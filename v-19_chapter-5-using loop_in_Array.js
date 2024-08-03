1b172770433c3a7b91d52bbdbe498a2be1279ab0
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-03 15:11:47 +0530

    for each loop

--- v-19_chapter-5-using loop_in_Array.js
+++ v-19_chapter-5-using loop_in_Array.js
@@ -11,3 +11,11 @@ for(i=0;i<num.length;i++){
     console.log(num [i]); //output : 1,2,3,4,5
 }
 
+//For Each Loop-     
+//               call a function onece for each array element
+//        suppose i have to multiply each array element to itself then we can use 'for each loop'.
+
+let num1 =[1,2,3,4,5];
+num1.forEach((Element)=>{ // calling function for each element onece time
+    console.log(Element * Element); //output - 1 ,4 ,9 ,16 ,25. 
+})
