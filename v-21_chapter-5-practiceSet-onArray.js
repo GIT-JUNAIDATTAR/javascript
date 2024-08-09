c301b1cc59feb5e9e5f1b79749ce860cc59664a3
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-09 20:40:33 +0530

    chapter 5 practice set  question nummber 1 completed

---
+++ v-21_chapter-5-practiceSet-onArray.js
@@ -0,0 +1,10 @@
+
+// 1.create an array of a number and take input from user to add  number to this  array 
+
+let arr = [10,20,30,40,50];
+let add_arr = prompt("enter array element to add in object--->>>");
+add_arr = parseInt(add_arr);
+arr . push(add_arr);
+console.log(arr);
+
+    
