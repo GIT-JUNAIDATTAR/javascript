c1fa463e2de00e14826d6bf1643ac482a418a7ca
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-11 10:37:50 +0530

    chapter 5 question 2 is solved

--- v-21_chapter-5-practiceSet-onArray.js
+++ v-21_chapter-5-practiceSet-onArray.js
@@ -7,4 +7,14 @@ add_arr = parseInt(add_arr);
 arr . push(add_arr);
 console.log(arr);
 
-    
+// 2 .keep adding array until 0 is not entered
+
+let arr2 = [1,2,3,4,5];
+let addarr2;
+do {
+     addarr2 = prompt("enter your array element to add in object");
+addarr2 = parseInt(addarr2);
+arr2.push(addarr2);
+console.log(arr2); } while(addarr2 != 0);
+console.log(arr2);
+ 
