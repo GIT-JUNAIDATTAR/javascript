85bf7424be32ed4807db2623b33eec4bf473d246
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-19 20:52:22 +0530

    console.time and timeEnd object of console in javascript

--- v-25_chapter-6_javascript-console-object.js
+++ v-25_chapter-6_javascript-console-object.js
@@ -6,3 +6,14 @@ console.info("important massage for you")//console.info- use for special informa
 console.warn("warning for you");//console.warn- use to give warning
 
 console.error("you got error");// console.error- use for give error
+
+console.assert(5>10);//console.assert- used to assert the condition
+ //( cheking the condition)
+
+ console.time("for loop"); //console.time- start counting time of for loop.
+
+ for (let i = 0; i < 50; i++) {
+    console.log(5)
+  }
+
+  console.timeEnd("for loop");//console.timeEnd- display the recorded time of for loop
