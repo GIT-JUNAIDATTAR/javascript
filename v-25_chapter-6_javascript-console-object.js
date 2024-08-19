3277b9458acdbde58f4777265920632d5e64314d
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-19 20:28:15 +0530

    console.warning object of console in javascript

--- v-25_chapter-6_javascript-console-object.js
+++ v-25_chapter-6_javascript-console-object.js
@@ -1,4 +1,6 @@
 
 console.log("hello"); // console.log- output a massage to console
 
-console.info("important massage for you")//console.info-use for special information
+console.info("important massage for you")//console.info- use for special information
+
+console.warn("warning for you");//console.warn- use to give warning
