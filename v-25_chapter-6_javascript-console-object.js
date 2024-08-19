aca4e37a463e88e51d643effcea3d639dc55fcd8
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-19 20:33:02 +0530

    console.error object of console in javascript

--- v-25_chapter-6_javascript-console-object.js
+++ v-25_chapter-6_javascript-console-object.js
@@ -4,3 +4,5 @@ console.log("hello"); // console.log- output a massage to console
 console.info("important massage for you")//console.info- use for special information
 
 console.warn("warning for you");//console.warn- use to give warning
+
+console.error("you got error");// console.error- use for give error
