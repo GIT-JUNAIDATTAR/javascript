ae9caf1071fd80e69e5fa6df66852345ebcf0a4d
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-21 20:16:51 +0530

    alert in javascript

--- v-26,27_combo_chapter-6_alert_confrim_prompt.js
+++ v-26,27_combo_chapter-6_alert_confrim_prompt.js
@@ -1,12 +1,16 @@
 
-//prompt in javscript
+//prompt in javscript -
 // Display a prompt dialog to the user
-var userInput = prompt("Please enter your name:"); //use to take input from user as string
+   var userInput = prompt("Please enter your name:"); //use to take input from user as string
 
-if (userInput !== null) {  // Check if the user entered something
+   if (userInput !== null) {  // Check if the user entered something
     console.log("Hello, " + userInput + "!");
-} else {
+   } 
+   else {
     console.log("User cancelled the prompt.");
-}
+   }
 
-
+//alert in javascript -
+// Display an alert with a variable
+var userName = "Alice";
+alert("Welcome, " + userName + "!"); //use to invoke mini window with a massage alert("hello")
