de60311ae7a7f9390b1ee336c5a5172fafa55ba0
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-21 19:53:54 +0530

    prompt in javascript

---
+++ v-26,27_combo_chapter-6_alert_confrim_prompt.js
@@ -0,0 +1,12 @@
+
+
+// Display a prompt dialog to the user
+var userInput = prompt("Please enter your name:"); //use to take input from user as string
+
+if (userInput !== null) {  // Check if the user entered something
+    console.log("Hello, " + userInput + "!");
+} else {
+    console.log("User cancelled the prompt.");
+}
+
+
