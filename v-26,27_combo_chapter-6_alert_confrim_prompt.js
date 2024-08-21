c15648ec147a69ce533a697a357e58f8480d6e57
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-21 20:58:39 +0530

    confirm in javascript

--- v-26,27_combo_chapter-6_alert_confrim_prompt.js
+++ v-26,27_combo_chapter-6_alert_confrim_prompt.js
@@ -14,3 +14,16 @@
 // Display an alert with a variable
 var userName = "Alice";
 alert("Welcome, " + userName + "!"); //use to invoke mini window with a massage alert("hello")
+document.write(userConfirmed); // document.write is use to display on webpage through javascript
+
+//confirm in javascript -
+// Display a confirmation dialog with a message
+var userConfirmed = confirm("Are you sure you want to proceed?"); //show a massage and wait for user
+                                                                 // to press ok or cancel return true 
+if (userConfirmed) {                                             //for ok and false for cancel
+    console.log("User clicked OK.");
+} else {
+    console.log("User clicked Cancel.");
+}
+
+//the exact location and look is ditermined by the browser whic is a limitation
