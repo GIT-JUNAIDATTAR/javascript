84298172c072b3097e6889701cebd92a3d0af46b
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-25 16:06:58 +0530

    DOM- documment object model info

---
+++ v-28_chapter-6_dom-bom-in-javascript/dom-bom.js
@@ -0,0 +1,13 @@
+
+/*
+   DOM(document object model)-
+              With the HTML DOM, JavaScript can access and change all the elements of an HTML document.
+              When a web page is loaded, the browser creates a Document Object Model of the page.
+             1> JavaScript can change all the HTML elements in the page
+             3>JavaScript can change all the CSS styles in the page
+             4>JavaScript can remove existing HTML elements and attributes
+             5>JavaScript can add new HTML elements and attributes
+             6>JavaScript can react to all existing HTML events in the page
+             7>JavaScript can create new HTML events in the page
+             2>JavaScript can change all the HTML attributes in the page
+*/
