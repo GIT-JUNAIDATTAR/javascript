25c0af8116c7fc28f4fc5cdc26dfb69954042cf3
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-25 16:20:19 +0530

    BOM ( browser object model ) info

--- v-28_chapter-6_dom-bom-in-javascript/dom-bom.js
+++ v-28_chapter-6_dom-bom-in-javascript/dom-bom.js
@@ -2,8 +2,9 @@
 /*
    DOM(document object model)-
               With the HTML DOM, JavaScript can access and change all the elements of an HTML document.
-              When a web page is loaded, the browser creates a Document Object Model of the page.
               
+              When a web page is loaded, the browser creates a Document Object Model of the page.
+
               With the object model, JavaScript gets all the power it needs to create dynamic HTML:
 
              1> JavaScript can change all the HTML elements in the page
@@ -13,4 +14,16 @@
              6>JavaScript can react to all existing HTML events in the page
              7>JavaScript can create new HTML events in the page
              2>JavaScript can change all the HTML attributes in the page
+*/
+
+/*
+   BOM(browser object model)-
+            The Browser Object Model (BOM) allows JavaScript to "talk to" the browser.
+
+            There are no official standards for the Browser Object Model (BOM).
+
+            Since modern browsers have implemented (almost) the same methods and 
+            properties for JavaScript interactivity, it is often referred to, as
+            methods and properties of the BOM.
+
 */
