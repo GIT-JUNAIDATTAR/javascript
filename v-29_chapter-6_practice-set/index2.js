530c0f76ef4e35247ced598df3be78f4d3869d9b
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-26 21:06:35 +0530

    question 2 chapter 6 practice set is solved

--- v-29_chapter-6_practice-set/index2.js
+++ v-29_chapter-6_practice-set/index2.js
@@ -2,3 +2,30 @@
 
 /* Q 2] In question nummber 1 use confirm  to ask the user if he want to see the prrompt again? */ 
 
+
+
+
+let runagain = true;
+  const candrive = (userinput) =>{
+    return userinput <=18 ?true:false;
+  }
+    
+while(runagain){
+  let userinput = prompt("ENTER YOUR AGE -->>");
+  userinput = Number.parseInt(userinput);
+  
+  if(candrive(userinput)){
+    alert("YOU CAN'T DRIVE");
+  }
+  else{
+    alert("YOU CAN DRIVE");
+  }
+  runagain = confirm("DO YOU WANT TO FILL AGAIN");
+}
+
+if(candrive(userinput)){
+  alert("YOU CAN'T DRIVE");
+}
+else{
+  alert("YOU CAN DRIVE");
+}
