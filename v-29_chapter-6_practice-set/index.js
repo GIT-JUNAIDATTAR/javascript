8f4811c5acf874063f78233745135f76d5e950cd
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-26 20:23:21 +0530

    question 1 chapter 6 ps solved

--- v-29_chapter-6_practice-set/index.js
+++ v-29_chapter-6_practice-set/index.js
@@ -2,3 +2,16 @@
 /* Q ]write a  program using prompt function to take input of age as a value from the user and use alert to
  tell him if he can drive or not
   */
+
+ let userinput = prompt("ENTER YOUR AGE -->>");
+userinput = Number.parseInt(userinput);
+
+const candrive = (userinput) =>{
+  return userinput <=18 ?true:false;
+}
+if(candrive(userinput)){
+  alert("YOU CAN'T DRIVE");
+}
+else{
+  alert("YOU CAN DRIVE");
+}
