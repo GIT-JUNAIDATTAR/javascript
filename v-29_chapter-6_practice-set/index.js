5a55ba00907358d0c4d1255b136ab3e90364b1da
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-26 20:57:24 +0530

    making new file for question 2 for sepration of code  and for better understanding

--- v-29_chapter-6_practice-set/index.js
+++ v-29_chapter-6_practice-set/index.js
@@ -15,5 +15,3 @@ if(candrive(userinput)){
 else{
   alert("YOU CAN DRIVE");
 }
-
-/* Q 2] In question nummber 1 use confirm  to ask the user if he want to see the prrompt again? */ 
---
+++ v-29_chapter-6_practice-set/index2.js
@@ -0,0 +1,4 @@
+
+
+/* Q 2] In question nummber 1 use confirm  to ask the user if he want to see the prrompt again? */ 
+
