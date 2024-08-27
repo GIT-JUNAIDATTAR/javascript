9a940b83c93fc9f914195925cfebb2fd5e87e693
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-27 16:38:32 +0530

    questionn number  4

---
+++ v-29_chapter-6_practice-set/index4.js
@@ -0,0 +1,5 @@
+
+/* Q 4] write a program to change the url to google.com (redirection) if user enter the number which 
+        was greater than 4                                                                       */
+        
+        
