8b6c408a6cd7d58226a14bf14fef7058fc8c8a0b
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-27 16:33:26 +0530

    chapter 6 prasctise set question 3 is solved

--- v-29_chapter-6_practice-set/index3.js
+++ v-29_chapter-6_practice-set/index3.js
@@ -1,2 +1,28 @@
 
 /* Q 3] in the privious question use console.error to log the error if the age enterd is nigative */ 
+
+
+
+
+let runagain = true;
+const candrive = (userinput) => {
+  return userinput <= 18 ? true : false;
+}
+
+while (runagain) {
+  let userinput = prompt("ENTER YOUR AGE -->>");
+  userinput = Number.parseInt(userinput);
+
+  if (userinput < 0) {
+    console.error("INVAILD AGE"); // this show error when age is entered in nigative intiger 
+    break;
+  }
+  if (candrive(userinput)) {
+    alert("YOU CAN'T DRIVE");
+  }
+  else {
+    alert("YOU CAN DRIVE");
+  }
+  runagain = confirm("DO YOU WANT TO FILL AGAIN");
+}
+
