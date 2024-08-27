a7ecd0ac98d66dd57216284df4d6a9cb798eee92
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-27 16:47:54 +0530

    queston number 4 chapter 6 practise set was solved

--- v-29_chapter-6_practice-set/index4.js
+++ v-29_chapter-6_practice-set/index4.js
@@ -2,4 +2,9 @@
 /* Q 4] write a program to change the url to google.com (redirection) if user enter the number which 
         was greater than 4                                                                       */
         
+        let userinput = prompt("ENTER YOUR NUMBEER");
+        userinput = Number.parseInt(userinput);
         
+        if (userinput > 4) {
+          location.href = "https://www.google.com"; // we redirect to the google.com through this link
+        } 
