abf9ec119818f1d36b31d4b36f41dfa76209d653
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-27 16:59:49 +0530

    question number 5 chapter 6 practice set is solved  and chapter 6 is over

--- v-29_chapter-6_practice-set/index5.js
+++ v-29_chapter-6_practice-set/index5.js
@@ -1,2 +1,9 @@
 
-/* Q 5] change the background color  based on user input throught prompt */ 
+/* Q 5] change the background color  based on user input throught prompt */
+
+
+ let usercolor = prompt("ENTER THE COLOR TO CHANGE THE BACKGROUND");
+
+window.document.body.style.background = (usercolor);
+
+// by this way we can change the background  according to the user input 
