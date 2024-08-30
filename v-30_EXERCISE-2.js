6893700b16585d180c0a30e7161b394a99cbe8fc
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-08-30 20:31:31 +0530

    commit

--- v-30_EXERCISE-2.js
+++ v-30_EXERCISE-2.js
@@ -1,4 +1,4 @@
  
  /* Q] use javascript to create a gamme of snake , water and gun . the game shouldd ask you to enter
        S,W or G . the computeer should be able to randomly generate S,W and G andd declare winn or loss 
-        using alert ,prompt and confirm whenever  required */
+       using alert ,prompt and confirm whenever  required */
