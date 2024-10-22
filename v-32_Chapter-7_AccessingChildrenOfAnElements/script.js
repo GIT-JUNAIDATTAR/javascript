4b6ef90c370d1dd69f69995365b1b13736269e8c
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-10-22 11:54:48 +0530

    commiting information about accessing the html elements,parent class ,child class from the javascript

---
+++ v-32_Chapter-7_AccessingChildrenOfAnElements/script.js
@@ -0,0 +1,10 @@
+// Document- convert all the html page in documment, then we can access the html through the javascript
+//           it was useful for intractive webpage
+// * javacsript is run on browser
+document.body; //for accesing the body of html
+
+document.body.firstChild; // for accessing the first child of body tag in html such as - HeaderTag
+
+document.body.lastChild; // for accessiing the last child of body tag in html such as - footer
+
+document.body.childNodes; // for accessing the all chiild of body tag in html
