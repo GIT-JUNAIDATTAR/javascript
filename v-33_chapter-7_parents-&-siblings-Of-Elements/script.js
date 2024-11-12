7e900527a8739c348ef0a4f387f152101c305112
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-11-12 19:41:54 +0530

    adding info about next/previous or right/left siblings

--- v-33_chapter-7_parents-&-siblings-Of-Elements/script.js
+++ v-33_chapter-7_parents-&-siblings-Of-Elements/script.js
@@ -9,3 +9,14 @@ document.body.firstElementChild.firstElementChild; // -- div.first
 //for accesing the second element child of the first element child
 
 document.body.firstElementChild.nextElementSibling; // -- div.second
+
+/*Note -  example of Next/Right and Previous/Left Siblings
+
+         <div>Some content</div>
+         <p>Some text</p>
+         
+         In the DOM tree, the siblings of an element are arranged in a linear fashion. The element that is to the right of the current element is called the next sibling and the element that is to the left of the current element is called the previous sibling. In the following HTML code, the <p> element is the next sibling of the <div> element and the <div> element is the previous sibling of the <p> element
+         
+         
+         
+         */
