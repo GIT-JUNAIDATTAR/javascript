610eedb3f08547d4904fbc193ff89981f4f09c62
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-11-12 19:16:05 +0530

    for accessing the child of an elements

--- v-33_chapter-7_parents-&-siblings-Of-Elements/script.js
+++ v-33_chapter-7_parents-&-siblings-Of-Elements/script.js
@@ -5,3 +5,7 @@ document.body.firstElementChild; // -- div
 // for accesing the first element chiild of first element child
 
 document.body.firstElementChild.firstElementChild; // -- div.first
+
+//for accesing the second element child of the first element child
+
+document.body.firstElementChild.nextElementSibling; // -- div.second
