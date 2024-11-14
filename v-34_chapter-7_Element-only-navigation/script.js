111387c561b6e83144b19701030f63ef6bd30f8d
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-11-14 11:45:43 +0530

    here we learn how we can access the elementchild  and manipulate it , this is simple example of it

---
+++ v-34_chapter-7_Element-only-navigation/script.js
@@ -0,0 +1,7 @@
+//making function for changing the background of first element child of body tag in html
+
+const changebg = () => {
+  document.body.firstElementChild.style.background = "red"; // here we learn how we can access the elementchild  and manipulate it , this is simple example of it
+};
+
+//  changebg();  // uncommit the function to change the background
