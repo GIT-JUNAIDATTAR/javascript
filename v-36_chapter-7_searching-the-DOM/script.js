d8bb8676d22f584649cff7adc0732b4fc549a951
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-12-16 11:57:27 +0530

    manipulation using id

---
+++ v-36_chapter-7_searching-the-DOM/script.js
@@ -0,0 +1,10 @@
+// DOM manipulation using the id (given inn html)
+
+let card1 = document.getElementById("card-1");
+card1.style.background = "#90ebf0";
+
+let card2 = document.getElementById("card-2");
+card2.style.background = "#31cc34";
+
+let card3 = document.getElementById("card-3");
+card3.style.background = "#b853cf";
