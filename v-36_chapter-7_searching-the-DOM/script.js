e8da62a637c20e4a437dced8d28c842afb224792
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-12-16 13:11:31 +0530

    /DOM manipulation using class(given in html)

--- v-36_chapter-7_searching-the-DOM/script.js
+++ v-36_chapter-7_searching-the-DOM/script.js
@@ -1,10 +1,15 @@
-// DOM manipulation using the id (given inn html)
+// DOM manipulation using the id (given in html)
 
 let card1 = document.getElementById("card-1");
 card1.style.background = "#90ebf0";
 
 let card2 = document.getElementById("card-2");
-card2.style.background = "#31cc34";
+card2.style.background = "#5ced64";
 
 let card3 = document.getElementById("card-3");
-card3.style.background = "#b853cf";
+card3.style.background = "#e67076";
+
+//DOM manipulation using class(given in html)
+
+let cardt = document.getElementsByClassName("card-title")[0]; // we can change the color of specific class like this
+cardt.style.color = "red";
