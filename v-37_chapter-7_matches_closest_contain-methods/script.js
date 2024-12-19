bf4298dee83f17ebae92bcd21ed525dc8733841e
JUNAID ATTAR <junnaidattar313@gmail.com>
2024-12-19 12:43:27 +0530

    closest() method and its practise and difffternt bitween matches and  closest

--- v-37_chapter-7_matches_closest_contain-methods/script.js
+++ v-37_chapter-7_matches_closest_contain-methods/script.js
@@ -19,3 +19,29 @@ if (container.matches(".first")) {
   console.log("not matched to selector"); //because- matches() checks if the element itself matches a specific CSS selector.
   // It doesn't look at child elements inside it.
 }
+
+// closest-
+//          it looks the nearest ancesstor,that matches the given css selector
+//          its also chek itself and make match until match was not found like- itself->parents class->grandParents class
+//          ↑ this is diffrent bitween matches and closest method
+if (container.closest(".first")) {
+  console.log(first);
+} else {
+  console.log("null");
+}
+
+if (first.closest(".container")) {
+  console.log(container);
+} else {
+  console.log("no closest");
+}
+
+if (first.closest(".first")) {
+  //  closest- chek itself(first class element) also
+  console.log(first);
+} else {
+  console.log("itself chek but not match");
+}
+
+
+//
